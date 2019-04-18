ae5b508edcd13cfca2605742873fdf6d2584594a
dealtek <dealtek@gmail.com>
2019-04-18 14:27:36 -0700

    v1

---
+++ header-inc-main.php
@@ -0,0 +1,3 @@
+<div class="box1 center1 boxblue" >
+<a href="dashboard.php" rel="external" data-role="button" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-home"><img src="pics/appbanner-320x60-main.png" width="280" height="60" class="center" ></a>
+</div>
---
+++ header-inc.php
@@ -0,0 +1,5 @@
+<div class="box1 center1 boxblue" >
+<a href="dashboard.php" rel="external"><img src="pics/appbanner-320x60-home1.png" width="320" height="61" class="center" alt=""></a>
+</div>
+
+<!--data-role="button" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-home" -->
